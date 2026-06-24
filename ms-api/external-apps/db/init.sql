55c8ee07392241d6c87889524bc7e1d8d3cd0585
IseAkira <akira.ise.m@gmail.com>
2026-06-24 12:55:43 +0900

    feat:external-appの作成in ms-api

--- ms-api/external-apps/db/init.sql
+++ ms-api/external-apps/db/init.sql
@@ -29,3 +29,22 @@ CREATE TABLE views (
 );
 
 INSERT INTO videos VALUES(1,'user1','title 1','2000-01-01 00:00:00', '2000-01-01 00:00:00')
+INSERT INTO videos VALUES(2, 'user_2', 'title 2', '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO videos VALUES(3, 'user_3', 'title 3', '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO videos VALUES(4, 'user_4', 'title 4', '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+
+
+INSERT INTO rates VALUES(1, 'user_1', 1, 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO rates VALUES(2, 'user_2', 2, 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO rates VALUES(3, 'user_3', 3, 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO rates VALUES(4, 'user_4', 4, 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+
+INSERT INTO views VALUES(1, 'user_1', 1, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO views VALUES(2, 'user_2', 2, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO views VALUES(3, 'user_3', 3, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+INSERT INTO views VALUES(4, 'user_4', 4, '2000-01-01 00:00:00', '2000-01-01 00:00:00');
+
+
+SELECT setval('videos_id_seq', (SELECT MAX(id) FROM videos));
+SELECT setval('rates_id_seq', (SELECT MAX(id) FROM rates));
+SELECT setval('views_id_seq', (SELECT MAX(id) FROM views));
