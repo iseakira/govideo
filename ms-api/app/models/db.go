2952fa7e8f239f105b51f9fc67907111ac5a0755
IseAkira <akira.ise.m@gmail.com>
2026-06-25 16:44:55 +0900

    feat:db接続をシングルトンで実装

---
+++ ms-api/app/models/db.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"fmt"
+	"log"
+
+	"ms-api/config"
+
+	"gorm.io/driver/postgres"
+
+	"gorm.io/gorm"
+)
+
+var DbConnection *gorm.DB
+
+func connectDb() (db *gorm.DB, err error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s dbname=%s password=%s sslmode=%s",
+		config.Config.DbHost,
+		config.Config.DbPort,
+		config.Config.DbUser,
+			config.Config.DbName,
+		config.Config.DbPassword,
+		config.Config.DbSslMode,
+
+	)
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
+
+func init() {
+	db,err := connectDb()
+	DbConnection = db
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
